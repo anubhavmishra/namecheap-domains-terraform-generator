c5ef8582c08f0e667aa458f2c51a8206cd5a1cf5
Anubhav Mishra <anubhavmishra@me.com>
2022-08-20 14:52:26 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/anubhavmishra/namecheap-domains-terraform-generator
+
+go 1.17
+
+require github.com/namecheap/go-namecheap-sdk/v2 v2.1.0
+
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/weppos/publicsuffix-go v0.15.0 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/namecheap/go-namecheap-sdk/v2/namecheap"
+)
+
+type Domain struct {
+	Name         string
+	Nameservers  []string
+	ResourceName string
+}
+
+var usageText = `Usage: namecheap-domains-terraform-generator {TERRAFORM_OUTPUT_FILE_NAME}
+eg: ./namecheap-domains-terraform-generator test.tf
+`
+
+var domainRecordsResource = `resource "namecheap_domain_records" "{{.ResourceName}}" {
+	domain = "{{.Name}}"
+	mode = "OVERWRITE"
+  
+	nameservers = [
+		{{range $i, $v := .Nameservers}}{{if $i}}{{printf ",\n		" }}{{end}}"{{.}}"{{end}}
+	]
+}
+
+`
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) < 1 {
+		fmt.Printf("error: output file name should be supplied\n\n")
+		fmt.Println(usageText)
+		os.Exit(1)
+	}
+	// Set terraform output file name
+	fileName := argsWithoutProg[0]
+
+	// Get namecheap environment variables
+	namecheapUsername := os.Getenv("NAMECHEAP_USER_NAME")
+	if namecheapUsername == "" {
+		fmt.Printf("error: namecheap username needs to be set using the 'NAMECHEAP_USER_NAME' environment variable\n\n")
+		os.Exit(1)
+	}
+	namecheapAPIUser := os.Getenv("NAMECHEAP_API_USER")
+	if namecheapAPIUser == "" {
+		fmt.Printf("error: namecheap api user needs to be set using the 'NAMECHEAP_API_USER' environment variable\n\n")
+		os.Exit(1)
+	}
+	namecheapAPIKey := os.Getenv("NAMECHEAP_API_KEY")
+	if namecheapAPIKey == "" {
+		fmt.Printf("error: namecheap api key needs to be set using the 'NAMECHEAP_API_KEY' environment variable\n\n")
+		os.Exit(1)
+	}
+	namecheapClientIP := os.Getenv("NAMECHEAP_CLIENT_IP")
+	if namecheapClientIP == "" {
+		fmt.Printf("error: namecheap client ip needs to be set using the 'NAMECHEAP_CLIENT_IP' environment variable\n\n")
+		os.Exit(1)
+	}
+	namecheapUseSandbox := os.Getenv("NAMECHEAP_USE_SANDBOX")
+	if namecheapUseSandbox == "" {
+		fmt.Printf("error: namecheap use sandbox api needs to be set using the 'NAMECHEAP_USE_SANDBOX' environment variable\n\n")
+		os.Exit(1)
+	}
+	namecheapUseSandboxBool, err := strconv.ParseBool(namecheapUseSandbox)
+	if err != nil {
+		fmt.Printf("error: couldn't convert 'NAMECHEAP_USE_SANDBOX' environment variable value to boolean: %v\n\n", err)
+		os.Exit(1)
+	}
+
+	// Create namecheap client
+	client := namecheap.NewClient(&namecheap.ClientOptions{
+		UserName:   namecheapUsername,
+		ApiUser:    namecheapAPIUser,
+		ApiKey:     namecheapAPIKey,
+		ClientIp:   namecheapClientIP,
+		UseSandbox: namecheapUseSandboxBool,
+	})
+
+	domains, err := client.Domains.GetList(nil)
+	if err != nil {
+		log.Fatalf("error getting a domain list from namecheap: %v\n", err)
+	}
+
+	// Validate file doesn't exist
+	if _, err := os.Stat(fileName); !errors.Is(err, os.ErrNotExist) {
+		fmt.Printf("error: file %q already exist\n\n", fileName)
+		os.Exit(1)
+	}
+
+	// create a new file
+	file, _ := os.Create(fileName)
+	defer file.Close()
+
+	for i, d := range *domains.Domains {
+		response, err := client.DomainsDNS.GetList(*d.Name)
+		if err != nil {
+			log.Fatalf("error listing domain name %q from namecheap: %v\n", *d.Name, err)
+		}
+		domain := Domain{
+			Name:         *d.Name,
+			Nameservers:  *response.DomainDNSGetListResult.Nameservers,
+			ResourceName: fmt.Sprintf("domain_%s_%d", strings.Replace(*d.Name, ".", "_", -2), i+1),
+		}
+		err = renderTemplate(&domain, file)
+		if err != nil {
+			log.Fatalf("error rendering domain resource: %v\n", err)
+		}
+	}
+}
+
+func renderTemplate(domain *Domain, f *os.File) error {
+	tmpl := template.New("domainRecordsResource")
+	tmp, err := tmpl.Parse(domainRecordsResource)
+	if err != nil {
+		return err
+	}
+	err = tmp.Execute(f, domain)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
